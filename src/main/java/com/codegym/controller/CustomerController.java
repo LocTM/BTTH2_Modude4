d8d7c6ca940ebf134cacaadcfc316b63b4488bc1
LocTM <tranminhloc.anhk@gmail.com>
2024-02-28 23:38:30 +0700

    cach 2 ok

--- src/main/java/com/codegym/controller/CustomerController.java
+++ src/main/java/com/codegym/controller/CustomerController.java
@@ -2,6 +2,7 @@ package com.codegym.controller;
 
 import com.codegym.model.Customer;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,6 +20,18 @@ public class CustomerController {
         customers.add( new Customer( "4","Tran Minh Loc4","Loc@gmail.com","0941424386"));
         request.setAttribute("customers", customers);
         return "customerList.jsp";
+    }
+
+    @GetMapping("dskh")
+    public String ds(Model model){
+        List<Customer> customers = new ArrayList<>();
+        customers.add( new Customer( "1","Tran Minh Lo1c","Loc@gmail.com","0941424386"));
+        customers.add( new Customer( "2","Tran Minh Loc2","Loc@gmail.com","0941424386"));
+        customers.add( new Customer( "3","Tran Minh Loc3","Loc@gmail.com","0941424386"));
+        customers.add( new Customer( "4","Tran Minh Loc4","Loc@gmail.com","0941424386"));
+        model.addAttribute("customers", customers);
+        return "customerList.jsp";
+
     }
 
 
